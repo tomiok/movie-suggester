7850ca1b8282c18290f92d4e3c843ea9a3a255a2
tomaslingotti <tomaslingotti@gmail.com>
2020-08-23 14:53:42 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tomiok/movies-suggester
+
+go 1.13
+
+require github.com/go-sql-driver/mysql v1.5.0
---
+++ internal/database/mysql_client.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MySqlClient struct {
+	*sql.DB
+}
+
+func NewMySQLClient() *MySqlClient {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/movies")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+
+	}
+
+}
