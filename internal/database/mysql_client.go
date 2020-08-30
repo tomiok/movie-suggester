c7fcae81dfd79d007676690dd6a81c186db96bdd
tomaslingotti <tomaslingotti@gmail.com>
2020-08-30 19:26:20 -0300

    little fix

--- internal/database/mysql_client.go
+++ internal/database/mysql_client.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/tomiok/movies-suggester/internal/logs"
 )
 
 type MySqlClient struct {
@@ -13,6 +14,7 @@ func NewMySQLClient() *MySqlClient {
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/movies")
 
 	if err != nil {
+		logs.Error("cannot create mysql client")
 		panic(err)
 	}
 
